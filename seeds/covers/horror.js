13b54ae7cc95eda0fd12f7ebed2b366470f6a26d
Sakshi <sakshiii.t17@gmail.com>
2026-04-14 23:11:19 +0530

    horror dataset

--- seeds/covers/horror.js
+++ seeds/covers/horror.js
@@ -1,26 +1,240 @@
 const horrorCovers = [
-  { title: "It", author: "Stephen King", image: { url: "https://covers.openlibrary.org/b/id/8228659-L.jpg" } },
-  { title: "The Shining", author: "Stephen King", image: { url: "https://covers.openlibrary.org/b/id/7264613-L.jpg" } },
-  { title: "Pet Sematary", author: "Stephen King", image: { url: "https://covers.openlibrary.org/b/id/8228658-L.jpg" } },
-  { title: "Carrie", author: "Stephen King", image: { url: "https://covers.openlibrary.org/b/id/7264612-L.jpg" } },
-  { title: "Doctor Sleep", author: "Stephen King", image: { url: "https://covers.openlibrary.org/b/id/7966788-L.jpg" } },
-  { title: "Mexican Gothic", author: "Silvia Moreno-Garcia", image: { url: "https://covers.openlibrary.org/b/id/10308488-L.jpg" } },
-  { title: "The Haunting of Hill House", author: "Shirley Jackson", image: { url: "https://covers.openlibrary.org/b/id/8406782-L.jpg" } },
-  { title: "We Have Always Lived in the Castle", author: "Shirley Jackson", image: { url: "https://covers.openlibrary.org/b/id/8406781-L.jpg" } },
-  { title: "Bird Box", author: "Josh Malerman", image: { url: "https://covers.openlibrary.org/b/id/7966787-L.jpg" } },
-  { title: "House of Leaves", author: "Mark Z. Danielewski", image: { url: "https://covers.openlibrary.org/b/id/8228657-L.jpg" } },
-  { title: "Dracula", author: "Bram Stoker", image: { url: "https://covers.openlibrary.org/b/id/8406780-L.jpg" } },
-  { title: "Frankenstein", author: "Mary Shelley", image: { url: "https://covers.openlibrary.org/b/id/8406779-L.jpg" } },
-  { title: "The Exorcist", author: "William Peter Blatty", image: { url: "https://covers.openlibrary.org/b/id/7264611-L.jpg" } },
-  { title: "Coraline", author: "Neil Gaiman", image: { url: "https://covers.openlibrary.org/b/id/8228656-L.jpg" } },
-  { title: "The Only Good Indians", author: "Stephen Graham Jones", image: { url: "https://covers.openlibrary.org/b/id/10308487-L.jpg" } },
-  { title: "The Silent Companions", author: "Laura Purcell", image: { url: "https://covers.openlibrary.org/b/id/8750248-L.jpg" } },
-  { title: "Hidden Pictures", author: "Jason Rekulak", image: { url: "https://covers.openlibrary.org/b/id/12803859-L.jpg" } },
-  { title: "A Head Full of Ghosts", author: "Paul Tremblay", image: { url: "https://covers.openlibrary.org/b/id/8749669-L.jpg" } },
-  { title: "My Best Friend's Exorcism", author: "Grady Hendrix", image: { url: "https://covers.openlibrary.org/b/id/10308486-L.jpg" } },
-  { title: "The Southern Book Club's Guide to Slaying Vampires", author: "Grady Hendrix", image: { url: "https://covers.openlibrary.org/b/id/10308485-L.jpg" } },
-  { title: "The Troop", author: "Nick Cutter", image: { url: "https://covers.openlibrary.org/b/id/8228655-L.jpg" } },
-  { title: "The Ritual", author: "Adam Nevill", image: { url: "https://covers.openlibrary.org/b/id/8228654-L.jpg" } },
+  {
+    title: "It",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14652285-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Girl Next Door",
+    author: "Jack Ketchum",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/641365-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Haunting of Hill House",
+    author: "Shirley Jackson",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/12684825-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Bird Box",
+    author: "Josh Malerman",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/8599267-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Only Good Indians",
+    author: "Stephen Graham Jones",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/10299432-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "We Have Always Lived in the Castle",
+    author: "Shirley Jackson",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14812400-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Pet Sematary",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14651682-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Hidden Pictures",
+    author: "Jason Rekulak",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/13185920-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Silent Companions",
+    author: "Laura Purcell",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/9251785-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Bunny",
+    author: "Mona Awad",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/15208714-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "We Spread",
+    author: "Iain Reid",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/12945100-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Troop",
+    author: "Nick Cutter",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14923181-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "A Head Full of Ghosts",
+    author: "Paul Tremblay",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/11950062-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Dracula",
+    author: "Bram Stoker",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/12622155-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Stolen Tongues",
+    author: "Felix Blackwell",
+    image: { url: "https://covers.openlibrary.org/b/id/10313692-M.jpg" },
+  },
+  {
+    title: "Misery",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/15151485-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Intercepts",
+    author: "T.J. Payne",
+    image: { url: "https://covers.openlibrary.org/b/id/13288027-M.jpg" },
+  },
+  {
+    title: "The Troop",
+    author: "Nick Cutter",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14923181-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Taking of Jake Livingston",
+    author: "Ryan Douglass",
+    image: { url: "https://covers.openlibrary.org/b/id/13746376-M.jpg" },
+  },
+  {
+    title: "House of Leaves",
+    author: "Mark Z. Danielewski",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/7273002-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Stand",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14914282-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Slewfoot",
+    author: "Brom",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/15128869-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  
+  {
+    title: "Doctor Sleep",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/14652972-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Deep",
+    author: "Nick Cutter",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/10034262-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Turn of the Screw",
+    author: "Henry James",
+    image: { url: "https://covers.openlibrary.org/b/id/15083863-L.jpg" },
+  },
+  {
+    title: "The Cabin at the End of the World",
+    author: "Paul Tremblay",
+    image: { url: "https://covers.openlibrary.org/b/id/14641922-M.jpg" },
+  },
+  {
+    title: "Rebecca",
+    author: "Daphne du Maurier",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/15049576-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Things Have Gotten Worse Since We Last Spoke",
+    author: "Eric LaRocca",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/11303322-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Penpal",
+    author: "Dathan Auerbach",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/10865100-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Little Stranger",
+    author: "Sarah Waters",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/10216792-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Come Closer",
+    author: "Sara Gran",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/12684864-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Ruins",
+    author: "Scott Smith",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/167453-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Mexican Gothic",
+    author: "Silvia Moreno-Garcia",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/10239163-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Shining",
+    author: "Stephen King",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/15090478-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "Coraline",
+    author: "Neil Gaiman",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/11438352-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  }
 ];
 
 module.exports = horrorCovers;
--- seeds/covers/romance.js
+++ seeds/covers/romance.js
@@ -1,32 +1,185 @@
 const romanceCovers = [
-  { title: "It Ends With Us", author: "Colleen Hoover", image: { url: "https://covers.openlibrary.org/b/id/10308498-L.jpg" } },
-  { title: "It Starts With Us", author: "Colleen Hoover", image: { url: "https://covers.openlibrary.org/b/id/13257523-L.jpg" } },
-  { title: "Verity", author: "Colleen Hoover", image: { url: "https://covers.openlibrary.org/b/id/12992070-L.jpg" } },
-  { title: "Ugly Love", author: "Colleen Hoover", image: { url: "https://covers.openlibrary.org/b/id/8743548-L.jpg" } },
-  { title: "Haunting Adeline", author: "H.D. Carlton", image: { url: "https://covers.openlibrary.org/b/id/13259272-L.jpg" } },
-  { title: "Twisted Love", author: "Ana Huang", image: { url: "https://covers.openlibrary.org/b/id/12803863-L.jpg" } },
-  { title: "Twisted Games", author: "Ana Huang", image: { url: "https://covers.openlibrary.org/b/id/12803864-L.jpg" } },
-  { title: "Twisted Hate", author: "Ana Huang", image: { url: "https://covers.openlibrary.org/b/id/12803865-L.jpg" } },
-  { title: "Twisted Lies", author: "Ana Huang", image: { url: "https://covers.openlibrary.org/b/id/13557087-L.jpg" } },
-  { title: "King of Wrath", author: "Ana Huang", image: { url: "https://covers.openlibrary.org/b/id/13897081-L.jpg" } },
-  { title: "The Love Hypothesis", author: "Ali Hazelwood", image: { url: "https://covers.openlibrary.org/b/id/12349040-L.jpg" } },
-  { title: "Love on the Brain", author: "Ali Hazelwood", image: { url: "https://covers.openlibrary.org/b/id/13104399-L.jpg" } },
-  { title: "The Spanish Love Deception", author: "Elena Armas", image: { url: "https://covers.openlibrary.org/b/id/12349039-L.jpg" } },
-  { title: "Icebreaker", author: "Hannah Grace", image: { url: "https://covers.openlibrary.org/b/id/13588072-L.jpg" } },
-  { title: "Better Than the Movies", author: "Lynn Painter", image: { url: "https://covers.openlibrary.org/b/id/10521091-L.jpg" } },
-  { title: "The Hating Game", author: "Sally Thorne", image: { url: "https://covers.openlibrary.org/b/id/8741441-L.jpg" } },
-  { title: "Beach Read", author: "Emily Henry", image: { url: "https://covers.openlibrary.org/b/id/10308495-L.jpg" } },
-  { title: "Book Lovers", author: "Emily Henry", image: { url: "https://covers.openlibrary.org/b/id/12992068-L.jpg" } },
-  { title: "People We Meet on Vacation", author: "Emily Henry", image: { url: "https://covers.openlibrary.org/b/id/10521090-L.jpg" } },
-  { title: "Happy Place", author: "Emily Henry", image: { url: "https://covers.openlibrary.org/b/id/13897082-L.jpg" } },
-  { title: "Archer's Voice", author: "Mia Sheridan", image: { url: "https://covers.openlibrary.org/b/id/8743547-L.jpg" } },
-  { title: "The Seven Husbands of Evelyn Hugo", author: "Taylor Jenkins Reid", image: { url: "https://covers.openlibrary.org/b/id/8749673-L.jpg" } },
-  { title: "Daisy Jones and The Six", author: "Taylor Jenkins Reid", image: { url: "https://covers.openlibrary.org/b/id/8750832-L.jpg" } },
-  { title: "Normal People", author: "Sally Rooney", image: { url: "https://covers.openlibrary.org/b/id/8750252-L.jpg" } },
-  { title: "Punk 57", author: "Penelope Douglas", image: { url: "https://covers.openlibrary.org/b/id/8743550-L.jpg" } },
-  { title: "Credence", author: "Penelope Douglas", image: { url: "https://covers.openlibrary.org/b/id/10308496-L.jpg" } },
-  { title: "The Fine Print", author: "Lauren Asher", image: { url: "https://covers.openlibrary.org/b/id/12349038-L.jpg" } },
-  { title: "Terms and Conditions", author: "Lauren Asher", image: { url: "https://covers.openlibrary.org/b/id/12803862-L.jpg" } },
+  {
+    title: "It Ends With Us",
+    author: "Colleen Hoover",
+    image: { url: "https://covers.openlibrary.org/b/id/15123232-L.jpg" },
+  },
+  {
+    title: "The Love Hypothesis",
+    author: "Ali Hazelwood",
+    image: { url: "https://covers.openlibrary.org/b/id/15161033-L.jpg" },
+  },
+
+  {
+    title: "Twisted Love",
+    author: "Ana Huang",
+    image: { url: "https://covers.openlibrary.org/b/id/15113434-M.jpg" },
+  },
+  {
+    title: "The Spanish Love Deception",
+    author: "Elena Armas",
+    image: { url: "https://covers.openlibrary.org/b/id/12649413-L.jpg" },
+  },
+  {
+    title: "Love on the Brain",
+    author: "Ali Hazelwood",
+    image: { url: "https://covers.openlibrary.org/b/id/12856642-L.jpg" },
+  },
+  {
+    title: "The Fine Print",
+    author: "Lauren Asher",
+    image: { url: "https://covers.openlibrary.org/b/id/12713595-L.jpg" },
+  },
+  {
+    title: "People We Meet on Vacation",
+    author: "Emily Henry",
+    image: { url: "https://covers.openlibrary.org/b/id/14567102-L.jpg" },
+  },
+  {
+    title: "Better Than the Movies",
+    author: "Lynn Painter",
+    image: { url: "https://covers.openlibrary.org/b/id/14357862-L.jpg" },
+  },
+  {
+    title: "Rewind It Back",
+    author: "Liz Tomforde",
+    image: { url: "https://covers.openlibrary.org/b/id/15115325-L.jpg" },
+  },
+  {
+    title: "Heartless Hunter",
+    author: "Kristen Ciccarelli",
+    image: { url: "https://covers.openlibrary.org/b/id/14725180-L.jpg" },
+  },
+  {
+    title: "Binding 13",
+    author: "Chloe Walsh",
+    image: { url: "https://covers.openlibrary.org/b/id/15091988-L.jpg" },
+  },
+  {
+    title: "Deep End",
+    author: "Ali Hazelwood",
+    image: { url: "https://covers.openlibrary.org/b/id/14840882-L.jpg" },
+  },
+  {
+    title: "Behind the Net",
+    author: "Stephanie Archer",
+    image: { url: "https://covers.openlibrary.org/b/id/15129866-L.jpg" },
+  },
+  {
+    title: "The Summer of Broken Rules",
+    author: "K.L. Walther",
+    image: { url: "https://covers.openlibrary.org/b/id/12410810-L.jpg" },
+  },
+  {
+    title: "The Fake Mate",
+    author: "Lana Ferguson",
+    image: { url: "https://covers.openlibrary.org/b/id/14715481-L.jpg" },
+  },
+  {
+    title: "Just for the Summer",
+    author: "Abby Jimenez",
+    image: { url: "https://covers.openlibrary.org/b/id/14602781-L.jpg" },
+  },
+  {
+    title: "Love Story",
+    author: "Erich Segal",
+    image: {
+      url: "https://covers.openlibrary.org/b/id/368925-M.jpg?default=https://openlibrary.org/static/images/icons/avatar_book-sm.png",
+    },
+  },
+  {
+    title: "The Pumpkin Spice Cafe",
+    author: "Laurie Gilmore",
+    image: { url: "https://covers.openlibrary.org/b/id/14823180-L.jpg" },
+  },
+  {
+    title: "Book Lovers",
+    author: "Emily Henry",
+    image: { url: "https://covers.openlibrary.org/b/id/15088975-M.jpg" },
+  },
+  {
+    title: "Twisted Hate",
+    author: "Ana Huang",
+    image: { url: "https://covers.openlibrary.org/b/id/13870257-M.jpg" },
+  },
+  {
+    title: "Things We Never Got Over",
+    author: "Lucy Score",
+    image: { url: "https://covers.openlibrary.org/b/id/15134455-L.jpg" },
+  },
+  {
+    title: "Archer’s Voice",
+    author: "Mia Sheridan",
+    image: { url: "https://covers.openlibrary.org/b/id/15099842-L.jpg" },
+  },
+
+  {
+    title: "Happy Place",
+    author: "Emily Henry",
+    image: { url: "https://covers.openlibrary.org/b/id/13757783-L.jpg" },
+  },
+  {
+    title: "The Unhoneymooners",
+    author: "Christina Lauren",
+    image: { url: "https://covers.openlibrary.org/b/id/15154162-L.jpg" },
+  },
+
+  {
+    title: "The Hating Game",
+    author: "Sally Thorne",
+    image: { url: "https://covers.openlibrary.org/b/id/15206889-L.jpg" },
+  },
+  {
+    title: "Every Summer After",
+    author: "Carley Fortune",
+    image: { url: "https://covers.openlibrary.org/b/id/15105890-L.jpg" },
+  },
+  {
+    title: "Before We Were Strangers",
+    author: "Renée Carlino",
+    image: { url: "https://covers.openlibrary.org/b/id/14851813-L.jpg" },
+  },
+  {
+    title: "The Kiss Quotient",
+    author: "Helen Hoang",
+    image: { url: "https://covers.openlibrary.org/b/id/14635947-L.jpg" },
+  },
+  {
+    title: "Icebreaker",
+    author: "Hannah Grace",
+    image: { url: "https://covers.openlibrary.org/b/id/14632721-L.jpg" },
+  },
+  {
+    title: "Terms and Conditions",
+    author: "Lauren Asher",
+    image: { url: "https://covers.openlibrary.org/b/id/13141007-L.jpg" },
+  },
+  {
+    title: "King of Pride",
+    author: "Ana Huang",
+    image: { url: "https://covers.openlibrary.org/b/id/15132867-L.jpg" },
+  },
+  {
+    title: "Me Before You",
+    author: "Jojo Moyes",
+    image: { url: "https://covers.openlibrary.org/b/id/15163093-L.jpg" },
+  },
+  {
+    title: "The Notebook",
+    author: "Nicholas Sparks",
+    image: { url: "https://covers.openlibrary.org/b/id/285902-L.jpg" },
+  },
+  
+  {
+    title: "Flawless",
+    author: "Elsie Silver",
+    image: { url: "https://covers.openlibrary.org/b/id/13735432-M.jpg" },
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    image: { url: "https://covers.openlibrary.org/b/id/8090214-L.jpg" },
+  },
 ];
 
 module.exports = romanceCovers;
